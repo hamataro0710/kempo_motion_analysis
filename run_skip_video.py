21f332f4ab7c80884d720118694f6d96f1c22b25
ryutaro <ryu.6171@gmail.com>
2019-01-27 16:05:24 +0900

    Add run_skip_video.py, it's modified from tf-pose-estimation to save figs & skip some frame

---
+++ run_skip_video.py
@@ -0,0 +1,77 @@
+# use tf-pose-estimation
+# https://github.com/ildoonet/tf-pose-estimation.git
+
+import argparse
+import logging
+import time
+
+import cv2
+import numpy as np
+
+from tf_pose.estimator import TfPoseEstimator
+from tf_pose.networks import get_graph_path, model_wh
+
+import subprocess
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger('TfPoseEstimator-Video')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+fps_time = 0
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
+    parser.add_argument('--video', type=str, default='')
+    parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
+    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin')
+    parser.add_argument('--show-process', type=bool, default=False,
+                        help='for debug purpose, if enabled, speed for inference is dropped.')
+    parser.add_argument('--showBG', type=bool, default=True, help='False to show skeleton only.')
+    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
+    args = parser.parse_args()
+
+    logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
+    w, h = model_wh(args.resolution)
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    cap = cv2.VideoCapture(args.video)
+    video_name = args.video.split('.')[0]
+
+    if cap.isOpened() is False:
+        print("Error opening video stream or file")
+    frame_no = 0
+    frame_devide = 5
+    frame_rate = 30 / frame_devide
+    while cap.isOpened():
+        ret_val, image = cap.read()
+        if not ret_val:
+            break
+        if (frame_no % frame_devide == 0):
+            # humans = e.inference(image)
+            humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+            print(humans)
+            print(frame_no)
+            if not args.showBG:
+                image = np.zeros(image.shape)
+            image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+            plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))            
+            bgimg = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2RGB)
+            bgimg = cv2.resize(bgimg, (e.heatMat.shape[1], e.heatMat.shape[0]), interpolation=cv2.INTER_AREA)
+            plt.savefig("../pics/" + video_name + "{0:06d}".format(frame_no) + ".png")
+        frame_no += 1
+        # cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        # cv2.imshow('tf-pose-estimation result', image)
+        # fps_time = time.time()
+        if cv2.waitKey(1) == 27:
+            break
+    cv2.destroyAllWindows()
+
+    subprocess.call(["ffmpeg", "-r", frame_rate, "-f", "image2", "-s", "1920x1080", "-i", "video_name%06d.png", "-vcodec", "libx264", "-crf", "25",  "-pix_fmt", "yuv420p", "../pics/"+video_name + "_pose.mp4"])
+
+logger.debug('finished+')
