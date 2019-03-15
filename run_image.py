911dc66a3cc8c329c6574e24fc754fdc546f2769
ryutaro <ryu.6171@gmail.com>
2019-03-15 09:52:52 +0900

    cog: debug list->np.array

--- run_image.py
+++ run_image.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     parser.add_argument('--image', type=str, default='./images/p1.jpg')
     parser.add_argument('--model', type=str, default='cmu', help='cmu / mobilenet_thin')
 
-    parser.add_argument('--resize', type=str, default='0x0',
+    parser.add_argument('--resize', type=str, default='"432x368"',
                         help='if provided, resize images before they are processed. '
                              'default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
     parser.add_argument('--resize-out-ratio', type=float, default=4.0,
